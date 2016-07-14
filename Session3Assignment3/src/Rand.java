9f5065bb172f3fcecebac1a8be5655ff987dc0c6
BhoomikaShashidhar <bhoomi.dt87@gmail.com>
2016-07-14 09:38:16 +0530

    session3asssignment3
    Randomn numbers

---
+++ Session3Assignment3/src/Rand.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class RandomNumbers {
+	  public static void main(String[] args) {
+	    int c;
+	    int random = (int)(Math.random()* 10 ) + 0;
+        System.out.println(random);
+	    }
+	  }
+	
